81579df9aea250631814fdf1a52b86d1633cd2e0
Harsh <hackwidharsh@gmail.com>
2023-01-09 23:43:13 +0530

    added json parsing for the weather data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -22,13 +23,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   getData() async {
+    String data;
     http.Response response = await http.get(Uri.parse(
         'https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=6876d62a7721420b46ca8ec09dafdb8a'));
     if (response.statusCode == 200) {
-      print(response.body);
+      data = response.body;
     } else {
       print(response.statusCode);
     }
+    var temperature = jsonDecode(data)['cod'];
+    print(temperature);
   }
 
   @override
